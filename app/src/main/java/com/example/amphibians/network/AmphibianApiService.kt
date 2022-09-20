abe88ac6f4033d099826c0bdebfe68589cf8ecbc
bryanares <bryanares12@gmail.com>
2022-09-20 20:07:27 +0300

    initial commit

---
+++ app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.amphibians.network
+
+//Base Url property
+private const val BASE_URL = "https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/"
+
+//Moshi object with kotlin adapter factory for Retrofit to use to parse JSON
+
+
+// TODO: Build a Retrofit object with the Moshi converter
+
+interface AmphibianApiService {
+    // TODO: Declare a suspended function to get the list of amphibians
+}
+
+// TODO: Create an object that provides a lazy-initialized retrofit service
+
